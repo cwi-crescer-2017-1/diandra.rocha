a308c05cb8b597b622db3aed5ec6033bd8f842fb
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-13 17:45:54 -0300

    feat(Armadura): implementação da classe Armadura

---
+++ Módulo1/Armadura.java
@@ -0,0 +1,9 @@
+public class Armadura{
+    private String constelacao;
+    private Categoria categoria;
+    
+    public Armadura(String constelacao, Categoria categoria){
+        this.constelacao = constelacao;
+        this.categoria = categoria;
+    }
+}
