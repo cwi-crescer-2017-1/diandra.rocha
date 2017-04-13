de086dde5e68a0c23a6b658999b72673d1c9bd06
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-13 17:44:38 -0300

    feat(Saint): implementação da classe Saint

---
+++ Módulo1/Saint.java
@@ -0,0 +1,28 @@
+public class Saint{
+    private String nome;
+    private Armadura armadura;
+    private boolean armaduraVestida;
+    private Genero genero = Genero.NAO_INFORMADO;
+    
+    public Saint(String nome, Armadura armadura){
+        this.nome = nome;
+        this.armadura = armadura; 
+    }
+    
+    public void vestirArmadura(){
+        this.armaduraVestida = true;
+    }
+    
+    //camelCase aAA
+    public boolean getArmaduraVestida(){
+        return this.armaduraVestida;
+    }
+    
+    public Genero getGenero(){
+        return this.genero;
+    }
+    public void setGenero(Genero genero){
+        this.genero = genero;
+    }
+    
+}
