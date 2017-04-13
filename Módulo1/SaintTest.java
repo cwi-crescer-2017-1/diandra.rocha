c3f1148afa51a8e57047e5aa2074975e6e3e099c
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-13 17:43:43 -0300

    test(SaintTest): Teste dos métodos da classe Saint

---
+++ Módulo1/SaintTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SaintTest{
+    @Test
+    public void vestirArmaduraDeixaArmaduraVestida(){
+        
+        //AAA
+        //1. Arrange - Montagem dos dados de teste
+        Armadura escorpiao = new Armadura("Escorpião", Categoria.OURO);
+        Saint milo = new Saint ("Milo", escorpiao);
+        
+        // 2.Act - Invocar a ação a ser testada
+        milo.vestirArmadura();
+        
+        //3.Assert - Verificação dos resultados de teste
+        boolean resultado = milo.getArmaduraVestida();
+        assertEquals(true,resultado);
+        
+    }
+    
+    @Test
+    public void naoVestirArmaduraDeixaArmaduraNaoVestida(){
+        Saint hyoga = new Saint ("Hyoga", new Armadura("Cisne",  Categoria.BRONZE));
+        assertEquals(false, hyoga.getArmaduraVestida());
+    }
+    
+    @Test
+    public void aoCriarSaintGeneroENaoInformado(){
+        Saint shaka = new Saint("Shaka", new Armadura("Virgem", Categoria.OURO));
+        
+        assertEquals(Genero.NAO_INFORMADO, shaka.getGenero());
+    }
+}
