875663fbed65747afa9dc8329559cda713b639d2
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-13 17:45:32 -0300

    feat(Categoria): implementação do Enum Categoria

---
+++ Módulo1/Categoria.java
@@ -0,0 +1,14 @@
+public enum Categoria{
+    OURO(3), PRATA(2), BRONZE(1);
+    
+    private int valor;
+    
+    private Categoria(int valor){
+        this.valor = valor;
+    }
+    
+    //dromedarioCase
+    public int getValor(){
+        return this.valor;
+    }
+}
