7296f2e1c57fe614d591a6627315fca1fd76af58
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-15 17:43:14 -0300

    refactor(Batalha): melhorar identação e métodos.

---
+++ Modulo01/Batalha.java
@@ -0,0 +1,20 @@
+public class Batalha {
+	
+    private Saint saint1;
+    private Saint saint2;
+    
+    public Batalha(Saint saint1, Saint saint2) {
+        this.saint1 = saint1;
+        this.saint2 = saint2;
+    }
+	
+    public void iniciar() {
+        if (saint1.getNivelArmadura() == saint2.getNivelArmadura() || 
+		    saint1.getNivelArmadura() > saint2.getNivelArmadura()) {
+            saint2.perderVida(10);
+        } else {
+            saint1.perderVida(10);	
+        }
+    }
+	
+}
