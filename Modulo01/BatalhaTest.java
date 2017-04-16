06f487749e6bde4d5ad46a453e3d954747640583
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-16 00:45:41 -0300

    refactor(BatalhaTest): melhorar identação do código.

--- Modulo01/BatalhaTest.java
+++ Modulo01/BatalhaTest.java
@@ -5,16 +5,16 @@ public class BatalhaTest {
     
     @Test
     public void iniciarRetiraVidaDoSaintCorreto() {
-		Saint mu = new Saint("Mu", new Armadura ("Áries", Categoria.OURO));
-		Saint dante = new Saint ("Dante", new Armadura("Cérbero", Categoria.PRATA));
-		Batalha battle = new Batalha(mu, dante);
+	Saint mu = new Saint("Mu", new Armadura ("Áries", Categoria.OURO));
+	Saint dante = new Saint ("Dante", new Armadura("Cérbero", Categoria.PRATA));
+	Batalha battle = new Batalha(mu, dante);
 		
-		battle.iniciar();    
-		double vidaMu = mu.getVida();
-		double vidaDante = dante.getVida();
+	battle.iniciar();    
+	double vidaMu = mu.getVida();
+	double vidaDante = dante.getVida();
            
-		assertEquals(100, vidaMu, 0.5);
-		assertEquals(90, vidaDante, 0.5);             
+	assertEquals(100, vidaMu, 0.5);
+	assertEquals(90, vidaDante, 0.5);             
     }
 	
     @Test
