89ec8d4f39f74a7fe7c29b5287181c3f9709a987
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-16 00:32:49 -0300

    refactor(Categoria): melhorar a identação do código.

---
+++ Modulo01/Categoria.java
@@ -0,0 +1,16 @@
+public enum Categoria {
+    OURO(3),
+	PRATA(2),
+	BRONZE(1);
+    
+    private int valor;
+    
+    private Categoria(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+	
+}
