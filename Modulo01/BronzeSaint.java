6a5f48a61be7a90bd84df5cfa5014f92f3c64bab
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-17 17:20:02 -0300

    feat(BronzeSaint): implementar filho da classe Saint.

---
+++ Modulo01/BronzeSaint.java
@@ -0,0 +1,8 @@
+public class BronzeSaint extends Saint{
+    
+    public BronzeSaint(String nome, Armadura armadura)throws Exception {
+        super(nome, armadura);
+        this.qtdSentidosDespertados = 5;
+    }
+    
+}
