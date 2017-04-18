1b8a98d1dfef8e0547e325f050ada0ae479840d6
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-18 01:18:19 -0300

    feat(Golpe): implementar a classe Golpe.

---
+++ Modulo01/Golpe.java
@@ -0,0 +1,11 @@
+public class Golpe {
+    
+    private String nome;
+    private int fatorDano;
+    
+    public Golpe(String nome, int fatorDano) {
+        this.nome = nome;
+        this.fatorDano = fatorDano;
+    }
+        
+}
