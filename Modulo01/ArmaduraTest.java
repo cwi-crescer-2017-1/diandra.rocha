d7e8a429e0e6bd493f0dff9e33a53201f5b14e0a
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-18 01:19:04 -0300

    test(ArmaduraTest): remover teste.

--- Modulo01/ArmaduraTest.java
+++ Modulo01/ArmaduraTest.java
@@ -2,23 +2,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ArmaduraTest {
-	
-	@Test
-	public void armaduraRecebeConstelacaoCorretamenteMesmoEmStringComMaisDeUmaPalavra() {
-		Armadura armaduraTest = new Armadura("Nome De Teste", Categoria.BRONZE);
-		
-		String constelacaoArmadura= armaduraTest.getConstelacao();
-		
-		assertEquals("Nome De Teste", constelacaoArmadura);
-	}
-	
-	@Test
-	public void armaduraRecebeCategoriaCorretamente() {
-		Armadura armaduraTest = new Armadura("NomeTeste", Categoria.BRONZE);
-		
-		Categoria categoriaArmaduraTest = armaduraTest.getCategoria();
-		
-		assertEquals(Categoria.BRONZE, categoriaArmaduraTest);
-	}
-	
+    
+    @Test
+    public void armaduraRecebeCategoriaCorretamente() {
+        Armadura armaduraTest = new Armadura(new Constelacao("Nome de Teste"),Categoria.BRONZE);
+        
+        Categoria categoriaArmaduraTest = armaduraTest.getCategoria();
+        
+        assertEquals(Categoria.BRONZE, categoriaArmaduraTest);
+    }
+    
 }
