50a8a2b7a06487f2202961fb43ebb1ca3ed63f15
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-18 01:17:19 -0300

    feat(Constelacao): implementar a classe Constelação.

---
+++ Modulo01/Constelacao.java
@@ -0,0 +1,31 @@
+public class Constelacao {
+    
+    private String nome;
+    private Golpe [] golpes = new Golpe[5];
+    
+    public Constelacao(String nome) {
+        this.nome = nome;
+    }
+    
+    public Constelacao (Golpe [] golpes) {
+        this.golpes = golpes;
+    }
+    
+     public Constelacao(String nome, Golpe [] golpes ) {
+        this.nome = nome;
+        this.golpes = golpes;
+    }
+    
+    public String getNomeConstelacao(){
+        return this.nome;
+    }
+    
+    public void adicionarGolpe(Golpe golpe) {
+        golpes[4] = golpe;
+    }
+    
+    public Golpe [] getGolpes() {
+        return this.golpes;
+    }
+    
+}
