15447923b29923f633342c17b0f6d8f262cdf124
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-19 02:32:24 -0300

    feat(ListaSaints): implementar classe ListaSaints.

---
+++ Modulo01/ListaSaints.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class ListaSaints {
+
+    private ArrayList <Saint> exercitoDeAthena = new ArrayList<>();
+    private double maior = 0;
+    private double menor = 999;
+
+    public ListaSaints () {
+    }
+
+    public ListaSaints (ArrayList<Saint> exercitoDeAthena){
+        this.exercitoDeAthena = exercitoDeAthena;
+    }
+
+    public ArrayList<Saint> getExercitoDeAthena() {
+        return this.exercitoDeAthena;
+    }
+
+    public void adicionarSaint(Saint saint) {
+        this.exercitoDeAthena.add(saint);
+    }
+
+    public Saint getSaint(int indice) {
+        return this.exercitoDeAthena.get(indice);
+    }
+
+    public void todos() {
+        for(Saint s : this.exercitoDeAthena) {
+            System.out.println(s.getNome());
+        }
+    }
+
+    public void remover(Saint saint) {
+        this.exercitoDeAthena.remove(saint);
+    }
+
+    public Saint buscarPorNome(String nome) {
+        for(Saint s : this.exercitoDeAthena) {
+            if(s.getNome().equalsIgnoreCase(nome)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Saint> buscarPorCategoria(Categoria categoria) {
+        ArrayList <Saint> sublistaCategoria = new ArrayList<>();
+        for(Saint s : this.exercitoDeAthena) {
+            if(s.getCategoriaArmadura() == categoria) {
+                sublistaCategoria.add(s);
+            }
+        }
+        return sublistaCategoria;
+    }
+
+    public ArrayList<Saint> buscarPorStatus(Status status) {
+        ArrayList <Saint> sublistaStatus = new ArrayList<>();
+        for(Saint s : this.exercitoDeAthena) {
+            if(s.getStatus() == status) {
+                sublistaStatus.add(s);
+            }
+        }
+        return sublistaStatus;
+    }
+
+    public Saint getSaintMaiorVida() {
+        Saint saintMaiorVida = new Saint();
+        for(Saint s : this.exercitoDeAthena) {
+            if(s.getVida() > maior){
+                maior = s.getVida();
+                saintMaiorVida = s;
+            }
+        }
+        return saintMaiorVida;
+    }
+
+    public Saint getSaintMenorVida() {
+        Saint saintMenorVida = new Saint();
+        for(Saint s : this.exercitoDeAthena) {
+            if(s.getVida() < menor){
+                menor = s.getVida();
+                saintMenorVida = s;
+            }
+        }
+        return saintMenorVida;
+    }
+}
