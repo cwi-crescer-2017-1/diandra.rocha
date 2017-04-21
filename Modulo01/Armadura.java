f1424b96d58b1540709855a01ec01df83b72f2cc
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-21 00:11:42 -0300

    refactor(Armadura): melhorar identação do código.

--- Modulo01/Armadura.java
+++ Modulo01/Armadura.java
@@ -1,19 +1,19 @@
 public class Armadura {
-	
+
     private Constelacao constelacao;
     private Categoria categoria;
-    
+
     public Armadura(Constelacao constelacao, Categoria categoria) {
         this.constelacao = constelacao;
         this.categoria = categoria;
     }
-	
+
     public Constelacao getConstelacao() {
-	return this.constelacao;
-	}
-    
+        return this.constelacao;
+    }
+
     public Categoria getCategoria() {
         return this.categoria;
     }
-	
+
 }
