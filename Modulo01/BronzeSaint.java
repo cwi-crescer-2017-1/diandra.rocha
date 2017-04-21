619d7ecce1982ac9826521276db83f56b56b9865
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-21 00:18:52 -0300

    feat(BronzeSaint): adicionar construtor específico.

--- Modulo01/BronzeSaint.java
+++ Modulo01/BronzeSaint.java
@@ -1,8 +1,12 @@
-public class BronzeSaint extends Saint{
-    
-    public BronzeSaint(String nome, Armadura armadura)throws Exception {
+public class BronzeSaint extends Saint {
+
+    public BronzeSaint(String nome, String constelacao) throws Exception {
+        this(nome, new Armadura(new Constelacao(constelacao), Categoria.BRONZE));
+    }
+
+    public BronzeSaint(String nome, Armadura armadura) throws Exception {
         super(nome, armadura);
         this.qtdSentidosDespertados = 5;
     }
-    
+
 }
