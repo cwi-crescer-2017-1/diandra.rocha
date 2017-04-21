c54d652b5d882e634f5bef6d07ad4074a3307b95
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-21 00:15:16 -0300

    refactor(Golpe): melhorar identação da classe e seus testes.

--- Modulo01/Golpe.java
+++ Modulo01/Golpe.java
@@ -1,26 +1,26 @@
 public class Golpe {
-    
+
     private String nome;
     private int fatorDano;
-    
+
     public Golpe(String nome, int fatorDano) {
         this.nome = nome;
         this.fatorDano = fatorDano;
     }
-    
+
     public String getNome() {
         return this.nome;
     }
-        
+
     public int getFatorDano() {
         return this.fatorDano;
     }
-    
-     public boolean equals(Object object) {
+
+    public boolean equals(Object object) {
         Golpe outroGolpe = (Golpe)object;        
-        
+
         return 
-            this.nome.equals(outroGolpe.nome)
-            && this.fatorDano == outroGolpe.fatorDano;
+        this.nome.equals(outroGolpe.nome)
+        && this.fatorDano == outroGolpe.fatorDano;
     }
 }
--- Modulo01/GolpeTest.java
+++ Modulo01/GolpeTest.java
@@ -3,14 +3,14 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 public class GolpeTest {
-    
+
     @Test
     public void comparandoDoisObjetosGolpeComMetodoEquals() {
         Golpe golpe = new Golpe ("Pó de Diamante", 33);
         Object object = new Golpe("Pó de Diamante", 33);
-        
+
         boolean resultado = golpe.equals(object);
-        
+
         assertEquals(true, resultado);
     }
 }
