e29a245962ff4cd928fff86be3a42557a32d4140
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-04-21 00:19:32 -0300

    feat(SilverSaint): adicionar construtor específico.

--- Modulo01/SilverSaint.java
+++ Modulo01/SilverSaint.java
@@ -1,8 +1,12 @@
 public class SilverSaint extends Saint {
-    
+
+    public SilverSaint(String nome, String constelacao) throws Exception {
+        this(nome, new Armadura(new Constelacao(constelacao), Categoria.PRATA));
+    }
+
     public SilverSaint (String nome, Armadura armadura) throws Exception {
         super(nome, armadura);
         this.qtdSentidosDespertados = 6;
     }
-    
+
 }
