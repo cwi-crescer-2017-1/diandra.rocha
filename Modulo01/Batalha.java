c34df1abbb06ad47342311e72c6e631bbdd0b77f
Diandra Rocha <diandra.rocha07@hotmail.com>
2017-04-25 01:16:29 -0300

    fix(Batalha): corrigir sistema de batalha por turnos.

--- Modulo01/Batalha.java
+++ Modulo01/Batalha.java
@@ -9,17 +9,31 @@ public class Batalha {
         this.saint2 = saint2;
     }
 
-    public void iniciar() throws Exception{
-        Golpear batalha;
-        do{
-            if (saint1.getNivelArmadura() >= saint2.getNivelArmadura()) {
-                batalha = new Golpear(this.saint1, this.saint2);
-                batalha.executar();
-            } else {
-                batalha = new Golpear(this.saint2, this.saint1);
-                batalha.executar();
-            }
-        }while(this.saint1.getVida()<0 && this.saint1.getStatus()!= Status.MORTO
-        ||this.saint2.getVida()<0 && this.saint2.getStatus()!= Status.MORTO);
+    public void iniciar() throws Exception {
+
+        int valor1 = this.saint1.getNivelArmadura();
+        int valor2 = this.saint2.getNivelArmadura();
+        final double dano = 10;
+        Saint saintEmAcao = null;
+
+        if (valor1 >= valor2) {
+            saintEmAcao = this.saint1;
+            this.saint2.perderVida(dano);
+        } else {
+            saintEmAcao = this.saint2;
+            this.saint1.perderVida(dano);
+        }
+
+        boolean nenhumMorto = true;
+        while (nenhumMorto) {
+
+            saintEmAcao = saintEmAcao == this.saint1 ? this.saint2 : this.saint1;
+
+            Movimento proximoMovimento = saintEmAcao.getProximoMovimento();
+            proximoMovimento.executar();
+
+            nenhumMorto = this.saint1.getStatus() != Status.MORTO && this.saint1.getVida() >0 &&
+            this.saint2.getStatus() != Status.MORTO && this.saint2.getVida()>0;
+        }
     }
 }
