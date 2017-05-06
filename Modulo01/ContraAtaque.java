c8574d087d032c20897d408768c35fd07d3638b7
Diandra Rocha <diandra.rocha07@hotmail.com>
2017-05-06 02:06:38 -0300

    fix(ModelagemIndividual): corrigir o arquivo.

---
+++ Modulo01/ContraAtaque.java
@@ -0,0 +1,17 @@
+public class ContraAtaque implements Movimento {
+    Saint golpeador;
+    Saint golpeado;
+
+    public ContraAtaque(Saint golpeador, Saint golpeado) {
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    
+    public void executar() {
+        
+        double vidaGolpeado = 
+    }
+    
+    
+
+}
