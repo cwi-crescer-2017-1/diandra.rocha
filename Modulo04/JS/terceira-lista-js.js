aa089637f15bfd4f06d98676d107987f5341a870
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-05-16 16:53:25 -0300

    fix(TerceiroExercicio): corrigir tirando o toFixed :)

--- Modulo04/JS/terceira-lista-js.js
+++ Modulo04/JS/terceira-lista-js.js
@@ -1,7 +1,11 @@
-function arredondar(numero, casas){
+function arredondamento(numero, casas){
     casas = casas || 2;
-    return numero.toFixed(casas) || numero.toFixed(2);
+    return parseFloat(numero.arredondar(casas) || numero.arredondar(2));
 }
 
-console.log(arredondar(12.4444, 3));
-console.log(arredondar(12.4444));
+Number.prototype.arredondar = function(decimals) {
+ return Math.round(this * Math.pow(10, decimals)) / (Math.pow(10, decimals)); 
+};
+
+console.log(arredondamento(12.4444, 3));
+console.log(arredondamento(12.4444));
