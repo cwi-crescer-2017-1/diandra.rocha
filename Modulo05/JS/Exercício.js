8899094cfef58f3c60cfc53fe7e5e5dde8631711
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-05-18 02:41:15 -0300

    feat(Exercício01): implementar busca por meio de filter.

--- Modulo05/JS/Exercício.js
+++ Modulo05/JS/Exercício.js
@@ -1,9 +1,46 @@
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp', []);
 
 
-myApp.controller("Controller", function($scope) {
-    $scope.pokemon = {
-        nome : "Bulbasaur",
-        tipo : "Unknow"
-    }
+myApp.controller("Controller", function ($scope) {
+    $scope.pokemons = [{
+            nome: "Venusaur",
+            tipo: "Grass"
+        },
+        {
+            nome: "Charizard",
+            tipo: "Fire"
+        },
+        {
+            nome: "Blastoise",
+            tipo: "Water"
+        },
+        {
+            nome: "Butterfree",
+            tipo: "Bug"
+        },
+        {
+            nome: "Beedrill",
+            tipo: "Poison"
+        },
+        {
+            nome: "Pidgeot",
+            tipo: "Flying"
+        },
+        {
+            nome: "Sandshrew",
+            tipo: "Ground"
+        },
+        {
+            nome: "Clefairy",
+            tipo: "Fairy"
+        },
+        {
+            nome: "Primeape",
+            tipo: "Fighting"
+        },
+        {
+            nome: "Alakazam",
+            tipo: "Psychic"
+        }
+    ]
 });
