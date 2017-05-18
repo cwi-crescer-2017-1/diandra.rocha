5ca4495c2f95de0fd80b9b95eb54039ad1603499
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-05-18 17:27:17 -0300

    feat(Exercício): implementar custom filter e corrigir problema no date.

--- Modulo05/JS/Exercício02.js
+++ Modulo05/JS/Exercício02.js
@@ -1,13 +1,56 @@
 var modulo = angular.module("Exercício02", []);
 
 modulo.controller("Controller", function ($scope) {
-
-$scope.click = click;
-
-function click(){
+    
+    function click(){
    dataObj = new Date($scope.data);  
    $scope.dataObj = dataObj;
    return dataObj;
 }
- 
-})
+
+ $scope.click = click;
+
+    var instrutores = [{
+    nome: 'Bernardo',
+    aula: [{
+        numero: 1,
+        nome: 'OO'
+      },
+      {
+        numero: 4,
+        nome: 'Javascript'
+      }
+    ]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
+  },
+  {
+    nome: 'Pedro (PHP)',
+    aula: [{
+      numero: 3,
+      nome: 'HTML e CSS'
+    }]
+  },
+  {
+    nome: 'Zanatta',
+    aula: [{
+      numero: 5,
+      nome: 'AngularJS'
+    }]
+  }
+];
+
+$scope.instrutores = instrutores;
+
+});
+
+modulo.filter('mascada', function (){
+    return function(nome){
+        return nome.replace(/Nunes/ig, '$Nunes$');
+    }
+});
