304f6b34fa899475de332b234b785569c9933c7d
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-05-19 03:40:27 -0300

    feat(Exercício03): em andamento.

---
+++ Modulo05/JS/Exercício03.js
@@ -0,0 +1,45 @@
+var modulo = angular.module("Exercício03", []);
+
+modulo.controller("Controller", function ($scope) {
+
+    let instrutores = [{
+            nome: 'Pedro (PHP)',
+            aula: 'HTML e CSS',
+            numero: 3
+        },
+        {
+            nome: 'Zanatta',
+            aula: 'AngularJS',
+            numero: 5
+        },
+        {
+            nome: 'Bernardo',
+            aula: 'OO',
+            numero: 1
+        },
+        {
+            nome: 'Bernardo',
+            numero: 4,
+            aula: 'Javascript'
+        },
+        {
+
+            nome: 'Nunes',
+            aula: 'Banco de Dados I',
+            numero: 2,
+        }
+    ];
+
+    modulo.filter('mascada', function () {
+        return function (nome) {
+            return nome.replace(/(nunes)/i, '$ $1 $');
+        }
+    });
+    modulo.filter('formata', function () {
+        return function (numero, aula) {
+            return "00" + numero + "  -  " + aula.toUpperCase();
+        }
+    });
+
+    $scope.instrutores = instrutores;
+});
