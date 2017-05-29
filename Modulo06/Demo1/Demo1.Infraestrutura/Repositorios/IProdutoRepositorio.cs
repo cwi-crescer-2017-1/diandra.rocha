863b90fb89c4f79e8b84b98d5f87a484923f0640
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-05-29 17:38:27 -0300

    feat(Crud): feito em aula

---
+++ Modulo06/Demo1/Demo1.Infraestrutura/Repositorios/IProdutoRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Demo1.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    namespace Demo1.Infraestrutura.Entidades
+    {
+        public interface IPedidoRepositorio
+        {
+            void Alterar(Pedido pedido);
+            void Criar(Pedido pedido);
+            void Excluir(int id);
+            IEnumerable<Pedido> Listar();
+            Pedido Obter(int id);
+        }
+    }
+}
