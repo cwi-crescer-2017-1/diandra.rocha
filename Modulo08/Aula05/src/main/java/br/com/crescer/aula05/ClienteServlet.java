eb75a2327586a3c3cbac5c9eab7d2e842e520fab
Diandra Rocha <diandra.rocha@cwi.com.br>
2017-06-26 17:29:39 -0300

    feat(Aula05): implementados em aula.

---
+++ Modulo08/Aula05/src/main/java/br/com/crescer/aula05/ClienteServlet.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author diandra.rocha
+ */
+public class ClienteServlet extends HttpServlet {
+    
+    private final List<String> lista = new ArrayList<>();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        try (final PrintWriter out = response.getWriter();) {
+            out.append("<!DOCTYPE html>");
+            out.append("<html>");
+            out.append("<head>");
+            out.append("<title>Java - aula5</title>");
+            out.append("<meta charset=\"UTF-8\">");
+            out.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            out.append("</head>");
+            out.append("<body>");
+            out.append("<form method='post'>");
+            out.append("<input>Nome</input>");
+            out.append("</form>");
+            out.append("</body>");
+            out.append("</html>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String nome = resp.getWriter().toString();
+        
+        if(nome != null){
+            lista.add(nome);
+        }
+    }
+    
+    
+
+}
